09f9e9e8b77b9b0de20adac63a87933fde31ed6d
unknown <98ds.stepanov@gmail.com>
2025-02-27 13:41:40 +0300

    фикс root

--- internal/handlers/root.go
+++ internal/handlers/root.go
@@ -14,5 +14,6 @@ func Root(c *gin.Context, st storage.Storage) {
 		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
-	c.JSON(http.StatusOK, string(jsonData))
+	c.Writer.Header().Add("Content-Type","text/html")
+	c.String(http.StatusOK, string(jsonData))
 }
