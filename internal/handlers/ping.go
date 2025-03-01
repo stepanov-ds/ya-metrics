29c7e369a65097d2416ab6ad13de9e0a61824724
unknown <98ds.stepanov@gmail.com>
2025-03-02 00:47:15 +0300

    логирование database_dsn

--- internal/handlers/ping.go
+++ internal/handlers/ping.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/stepanov-ds/ya-metrics/internal/config/server"
 	"github.com/stepanov-ds/ya-metrics/internal/logger"
 	"go.uber.org/zap"
 )
@@ -15,7 +15,12 @@ func Ping(c *gin.Context, pool *pgxpool.Pool) {
 	err := pool.Ping(context.Background())
 	if err != nil {
 		println(err.Error())
-		logger.Log.Info("Connections string: ", zap.String("Database_DSN", *server.Database_DSN))
+		logger.Log.Info("Connection configuration: ", 
+			zap.String("Host", pool.Config().ConnConfig.Host),
+			zap.String("Port", strconv.FormatInt(int64(pool.Config().ConnConfig.Port), 10)),
+			zap.String("User", pool.Config().ConnConfig.User),
+			zap.String("Database", pool.Config().ConnConfig.Database),
+		)
 		c.String(http.StatusInternalServerError, "")
 	} else {
 		c.String(http.StatusOK, "")
